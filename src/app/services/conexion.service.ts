e33741a72312815bc37fc09535bcc1102c6fbeae
idkpaula <paula701ag@alumnes.ilerna.com>
2025-05-12 11:15:26 +0200

    master

--- src/app/services/conexion.service.ts
+++ src/app/services/conexion.service.ts
@@ -13,12 +13,12 @@ export class ConexionService {
 
   // Obtenemos los datos de la ruta prueba de la api para comprovar que la conexión con Laravel es exitosa
   getDatos(): Observable<any> {
-    return this.http.get('http://localhost:8000/api/prueba');
+    return this.http.get(`${this.apiUrl}/prueba`);
   }
 
   // Método para enviar los datos del registro a la base de datos
   registerUsuario(data: any): Observable<any> {
-    return this.http.post('http://localhost:8000/api/register', data);
+    return this.http.post(`${this.apiUrl}/register`, data);
   }
 
   loginUsuario(data: any): Observable<any> {
